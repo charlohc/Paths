e0e0e8bfd3e6eb18e7d559fba3bfc76ab749dff3
charlohc <Charlottee.Christiansen@gmail.com>
2023-03-04 14:32:09 +0100

    Created the Player class, updated the action class to take in player object

--- src/main/java/edu/ntnu/paths/Actions/Action.java
+++ src/main/java/edu/ntnu/paths/Actions/Action.java
@@ -1,5 +1,7 @@
 package edu.ntnu.paths.Actions;
 
+import edu.ntnu.paths.GameDetails.Player;
+
 public interface Action {
-   /* public void execute(Player player);*/
+   public void execute(Player player);
 }
---
+++ src/main/java/edu/ntnu/paths/GameDetails/Player.java
@@ -0,0 +1,59 @@
+package edu.ntnu.paths.GameDetails;
+
+import java.util.List;
+
+public class Player {
+    private final String name;
+    private int health;
+    private int score;
+    private int gold;
+    private List<String> inventory;
+
+    public Player(String name, int health, int score, int gold) {
+        this.name = name;
+        this.health = health;
+        this.score = score;
+        this.gold = gold;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void addHealth(int health) {
+        if (health <= 0) throw new IllegalArgumentException("Cannot add negative or zero number to health");
+        this.health += health;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void addScore(int score) {
+        if (score <= 0) throw new IllegalArgumentException("Cannot add negative or zero number to score");
+        this.score += score;
+    }
+
+    public int getGold() {
+        return gold;
+    }
+
+    public void addGold(int gold) {
+        if (gold <= 0) throw new IllegalArgumentException("Cannot add negative or zero number to gold");
+        this.gold += gold;
+    }
+
+    public List<String> getInventory() {
+        return inventory;
+    }
+
+    public void addToInventory(String item) {
+        if (item.isEmpty()) throw new IllegalArgumentException("Item cannot be blank");
+        if (inventory.contains(item)) throw new IllegalArgumentException("Item is already in inventory");
+        inventory.add(item);
+    }
+}
