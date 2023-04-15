10babe5fb22c7075c99b0d3e2b7a1e5836b07046
charlohc <Charlottee.Christiansen@gmail.com>
2023-04-16 00:43:30 +0200

    Created ReadFileTest class

---
+++ src/test/java/edu/ntnu/paths/FileHandling/ReadFileTest.java
@@ -0,0 +1,32 @@
+package edu.ntnu.paths.FileHandling;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReadFileTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    //Todo: test for at hvis man skriver inn spesial tegn, feks :: i navnet, skal den kaste en exception, samme for passage content og reference og action
+
+    //Todo: teste for at hvis noe mangler skal kaste exception, feks mangler navn, mangler content, mangler noe
+
+    //todo: i passage (Spesial tegn, har content men ikke text osv...)
+
+    //todo: Teste at det som det leser returnerer et ordentlig objekt osv..
+
+    //todo: teste med forskjellige histori typer, lange korte osv..
+
+    //todo: teste hva hvis det er to like passager feks
+
+    //todo: teste hvis det er en tom linje midt i en passage
+
+    //todo: teste action greiene satt opp feil
+
+    //todo: teste flere av samme action
+
+}
