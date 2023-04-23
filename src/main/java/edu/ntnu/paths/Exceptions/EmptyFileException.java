1608cccd631ad54713c7e40dcb24ba49710be468
Charlohc <charlohc@stud.ntnu.no>
2023-04-23 22:22:48 +0200

    Implemented custom exception EmptyFileException

---
+++ src/main/java/edu/ntnu/paths/Exceptions/EmptyFileException.java
@@ -0,0 +1,8 @@
+package edu.ntnu.paths.Exceptions;
+
+public class EmptyFileException extends StoryExistException {
+    public EmptyFileException(String errorMessage) {
+        super(errorMessage);
+    }
+
+}
