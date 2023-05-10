fe28c00bfb26ec9d6f1bff73cd0eb4fbb2e54948
Charlohc <charlohc@stud.ntnu.no>
2023-05-10 23:53:54 +0200

    Implemented the CreateGoals controller and fxml file

---
+++ src/main/java/edu/ntnu/paths/Controller/CreateGoals.java
@@ -0,0 +1,58 @@
+package edu.ntnu.paths.Controller;
+
+import edu.ntnu.paths.GameDetails.Player;
+import edu.ntnu.paths.Managers.PlayerManager;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class CreateGoals {
+
+    @FXML
+    private ComboBox<String> dropdown;
+
+    @FXML
+    private Label contentLabel;
+
+    @FXML
+    private void initialize() {
+        dropdown.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            handleDropdownChange(newValue);
+        });
+    }
+
+    private void handleDropdownChange(String selectedOption) {
+        switch (selectedOption) {
+            case "Gold" -> contentLabel.setText("Gold Goal");
+            case "Health" -> contentLabel.setText("Health Goal");
+            case "Inventory" -> contentLabel.setText("Inventory Goal");
+            case "Score" -> contentLabel.setText("Score Goal");
+        }
+    }
+    @FXML
+    private void importStory(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("import-story.fxml"));
+        Parent importedStory = loader.load();
+        Scene importedStoryScene = new Scene(importedStory, 1000, 600);
+
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        currentStage.setScene(importedStoryScene);
+        currentStage.show();
+    }
+    @FXML
+    private void goBack(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("create-user.fxml"));
+        Parent importedStory = loader.load();
+        Scene importedStoryScene = new Scene(importedStory, 1000, 600);
+
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        currentStage.setScene(importedStoryScene);
+        currentStage.show();
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module edu.ntnu.idatt2001.paths_v2 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.prefs;
 
     exports edu.ntnu.paths.Controller;
     opens edu.ntnu.paths.Controller to javafx.fxml;
