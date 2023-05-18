ed7ec7e2628dbc6f810ca6b5b8b105b72098d1bd
charlohc <Charlottee.Christiansen@gmail.com>
2023-05-19 00:31:43 +0200

    added more exception handling

--- src/main/java/edu/ntnu/paths/Goals/InventoryGoal.java
+++ src/main/java/edu/ntnu/paths/Goals/InventoryGoal.java
@@ -2,11 +2,15 @@ package edu.ntnu.paths.Goals;
 
 import edu.ntnu.paths.GameDetails.Player;
 
+import java.util.ArrayList;
 import java.util.List;
 public class InventoryGoal implements Goal{
     private List<String> inventory;
 
     public List<String> getInventory() {
+        if (inventory == null) {
+            return new ArrayList<>();
+        }
         return inventory;
     }
     public void inventoryGoal(List<String> mandatoryItems) {
