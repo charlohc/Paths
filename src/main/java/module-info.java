766ddad3356667c2eae46ed304b6e156f275fc98
charlohc <Charlottee.Christiansen@gmail.com>
2023-05-19 00:32:13 +0200

    made another export

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,5 +5,6 @@ module edu.ntnu.idatt2001.paths_v2 {
 
     exports edu.ntnu.paths.Controller;
     exports edu.ntnu.paths.Goals;
+    exports edu.ntnu.paths.GameDetails;
     opens edu.ntnu.paths.Controller to javafx.fxml;
 }
