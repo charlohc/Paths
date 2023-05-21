8e683c7054e809eba8ba5a39088e1d683cde607f
Charlohc <charlohc@stud.ntnu.no>
2023-05-21 20:00:49 +0200

    implemented JavaDoc

--- src/main/java/edu/ntnu/paths/Actions/Action.java
+++ src/main/java/edu/ntnu/paths/Actions/Action.java
@@ -2,6 +2,10 @@ package edu.ntnu.paths.Actions;
 
 import edu.ntnu.paths.GameDetails.Player;
 
+/**
+ * Action Interface class
+ * Represents an action happening when following a link to a passage
+ */
 public interface Action {
    public boolean execute(Player player);
 
