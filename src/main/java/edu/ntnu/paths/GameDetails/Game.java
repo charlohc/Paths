850d765452915a7fdce7b3d3d4fe3e2097915838
Charlohc <charlohc@stud.ntnu.no>
2023-05-21 21:47:36 +0200

    Added JavaDoc

--- src/main/java/edu/ntnu/paths/GameDetails/Game.java
+++ src/main/java/edu/ntnu/paths/GameDetails/Game.java
@@ -1,6 +1,5 @@
 package edu.ntnu.paths.GameDetails;
 
-
 import edu.ntnu.paths.StoryDetails.Link;
 import edu.ntnu.paths.StoryDetails.Passage;
 import edu.ntnu.paths.StoryDetails.Story;
@@ -10,23 +9,37 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+/**
+ * Represents a game instance.
+ */
 public class Game {
     private final Player player;
     private final Story story;
     private final List<Goal> goals;
 
+    /**
+     * Constructs a Game object using the GameBuilder.
+     *
+     * @param gameBuilder The GameBuilder object to construct the Game from.
+     * @throws NullPointerException if the player or story is null.
+     */
     public Game(GameBuilder gameBuilder) {
-        if (gameBuilder.player == null) { throw new NullPointerException("Player cannot be null");
-        }
-        else if (gameBuilder.story == null) { throw new NullPointerException("Story cannot be null");
-        }
-        else {
+        if (gameBuilder.player == null) {
+            throw new NullPointerException("Player cannot be null");
+        } else if (gameBuilder.story == null) {
+            throw new NullPointerException("Story cannot be null");
+        } else {
             this.player = gameBuilder.player;
             this.story = gameBuilder.story;
             this.goals = gameBuilder.goals;
         }
     }
 
+    /**
+     * Constructs a copy of a Game object.
+     *
+     * @param copyGame The Game object to copy.
+     */
     public Game(Game copyGame) {
         this.player = new Player(copyGame.getPlayer());
         this.story = new Story(copyGame.getStory());
@@ -39,28 +52,49 @@ public class Game {
         }
     }
 
-
+    /**
+     * Returns the player of the game.
+     *
+     * @return The player object.
+     */
     public Player getPlayer() {
         return player;
     }
 
+    /**
+     * Returns the story of the game.
+     *
+     * @return The story object.
+     */
     public Story getStory() {
         return story;
     }
 
+    /**
+     * Returns the list of goals in the game.
+     *
+     * @return The list of goals.
+     */
     public List<Goal> getGoals() {
         return goals;
     }
 
-
-
+    /**
+     * Begins the game by returning the initial passage of the story.
+     *
+     * @return The initial passage of the story.
+     */
     public Passage begin() {
         return story.getPassage();
     }
 
+    /**
+     * Continues the game by following the provided link and returning the next passage.
+     *
+     * @param link The link to follow.
+     * @return The next passage based on the chosen link.
+     */
     public Passage go(Link link) {
         return story.getPassage(link);
-
     }
-
 }
--- src/main/java/edu/ntnu/paths/GameDetails/GameBuilder.java
+++ src/main/java/edu/ntnu/paths/GameDetails/GameBuilder.java
@@ -7,33 +7,64 @@ import edu.ntnu.paths.StoryDetails.Story;
 
 import java.util.List;
 
+/**
+ * Builder class for constructing a Game object.
+ */
 public class GameBuilder {
 
     public Player player;
     public Story story;
     public List<Goal> goals;
 
+    /**
+     * Creates a new instance of GameBuilder.
+     *
+     * @return The GameBuilder instance.
+     */
     public static GameBuilder newInstance() {
         return new GameBuilder();
     }
 
     private GameBuilder() {}
 
-
+    /**
+     * Sets the player for the game.
+     *
+     * @param player The player object.
+     * @return The GameBuilder instance.
+     */
     public GameBuilder setPlayer(Player player) {
         this.player = player;
         return this;
     }
 
+    /**
+     * Sets the story for the game.
+     *
+     * @param story The story object.
+     * @return The GameBuilder instance.
+     */
     public GameBuilder setStory(Story story) {
         this.story = story;
         return this;
     }
 
+    /**
+     * Sets the goals for the game.
+     *
+     * @param goals The list of goals.
+     * @return The GameBuilder instance.
+     */
     public GameBuilder setGoals(List<Goal> goals) {
         this.goals = goals;
         return this;
     }
+
+    /**
+     * Builds a new Game object using the provided configurations.
+     *
+     * @return The constructed Game object.
+     */
     public Game build() {
         return new Game(this);
     }
